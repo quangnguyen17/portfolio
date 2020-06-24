1c9838b3b58c6ec262b029e36ed2c8705d47b7e9
Quang Nguyen <wan15112001@gmail.com>
2020-06-23 23:16:49 -0700

    all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Button, Card } from "react-bootstrap";
+import "./App.css";
 
-function App() {
+const SERVER_URL = "https://quangnguyen-server.herokuapp.com/api";
+
+const App = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/resume?projects`)
+      .then(({ data }) => setProjects(data.results))
+      .catch(console.error);
+  }, []);
+
+  const Header = () => {
+    return (
+      <>
+        <h1>Quang Nguyen</h1>
+        <p>I'm a:</p>
+      </>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div class="scrolling-wrapper-flexbox py-3">
+        {projects.map((project, index) => (
+          <Card key={index} style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
